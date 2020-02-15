c5b049cf921ca0e5ca14ab46e3513d07556cfb4d
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2020-02-15 16:24:38 +0100

    refacotring

--- scripts/create_content.py
+++ scripts/create_content.py
@@ -6,8 +6,10 @@ import os
 
 if __name__ == '__main__':
 
+    os.chdir(os.environ["PROJECT_DIR"] + "/handout")
+
     [os.system(type_ + " main") for type_ in ["pdflatex", "bibtex", "pdflatex", "pdflatex"]]
 
-    shutil.move("main.pdf", "handout-eckstein-keane-wolpin.pdf")
+    shutil.move("main.pdf", "../handout-eckstein-keane-wolpin-models.pdf")
 
     sp.check_call(["git", "clean", "-xdf"])
